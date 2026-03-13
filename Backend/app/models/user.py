01b9c56ff1ae7088ae40ad76cabf8bd8ca164668
Martin Silungwe <martinsilungwe12@gmail.com>
2026-03-13 11:31:44 +0200

    create the users table

---
+++ Backend/app/models/user.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, Field
+import uuid
+from datetime import datetime
+
+
+class User(SQLModel, table=True):
+
+    __tablename__ = "users"
+    user_id: uuid.UUID = Field(uuid.uuid4(), primary_key=True)
+    fullname: str = Field(nullable=False, index=True)
+    email: str = Field(nullable=False, index=True)
+    hashed_password: str = Field(nullable=False, index=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now())
+    updated_at: datetime = Field(default_factory=lambda: datetime.now())

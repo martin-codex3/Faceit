78fdb45243191920cdf3a3ac898326e4a8e9fc10
Martin Silungwe <martinsilungwe12@gmail.com>
2026-03-13 06:00:56 +0200

    adding database driver

--- Backend/app/config/app_config.py
+++ Backend/app/config/app_config.py
@@ -4,7 +4,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class AppConfig(BaseSettings):
 
     DATABASE_URL: str | None = None
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
+    DATABASE_PORT: int
+    DATABASE_HOST: str
 
     model_config = SettingsConfigDict(
         env_file=".env"
     )
+
+    # we will get the connection here
+    def get_connection_string(self) -> str:
+        connection_string = f""

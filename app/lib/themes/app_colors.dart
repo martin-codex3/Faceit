1e9a319efaff5b27402fffc075479947b95021c1
Martin Silungwe <martinsilungwe12@gmail.com>
2026-03-14 17:47:16 +0200

    adding app theme

---
+++ app/lib/themes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryGreen = Color(0xff0ae448);
+  static Color primaryBlack = Color(0xff0e100f);
+  static Color primaryWhite = Color(0xfffffce1);
+}
---
+++ app/lib/themes/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:app/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.primaryBlack,
+    primaryColor: AppColors.primaryGreen,
+  );
+}

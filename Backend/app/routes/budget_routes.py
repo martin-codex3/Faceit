7bbf9fb3387ce7425d760546a807c09005910b0b
Martin Silungwe <martinsilungwe12@gmail.com>
2026-03-16 11:13:51 +0200

    working budget routes

--- Backend/app/routes/budget_routes.py
+++ Backend/app/routes/budget_routes.py
@@ -8,6 +8,21 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 budget_router = APIRouter()
 budget_service = BudgetService()
 
+
+# we will attempt to fetch all the records here
 @budget_router.get("/", status_code=status.HTTP_200_OK, response_model=CreateBudgetSchema)
 async def get_all_budgets(session: AsyncSession = Depends(app_session)):
+    all_budget = await budget_service.get_all_budgets(
+        session = session
+    )
+
+    if all_budget is not None:
+        return all_budget
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Records not found")
+
+
+# we will attempt to create a new record here
+@budget_router.post("/create_budget", status_code=status.HTTP_201_CREATED)
+async def create_new_budget(budget_data: CreateBudgetSchema, session: AsyncSession = Depends(app_session)):
     
--- Backend/app/services/budget_service.py
+++ Backend/app/services/budget_service.py
@@ -1,9 +1,16 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.models.budget import BudgetModel
 from app.schemas.budget_schema import CreateBudgetSchema
-
+from sqlmodel import select, desc
 
 class BudgetService:
+
+    # for fetching all the records
+    async def get_all_budgets(self, session: AsyncSession):
+        statement = select(BudgetModel).order_by(desc(BudgetModel.created_at))
+        results = await session.exec(statement)
+        return results.all()
+
 
     async def create_budget(self, budget_data: CreateBudgetSchema, session: AsyncSession):
         budget = budget_data.model_dump()

ad19ecd1127dcb1150fb7b1c6ec451151c330216
Martin Silungwe <martinsilungwe12@gmail.com>
2026-03-16 11:05:13 +0200

    working on budget routes

--- Backend/app/routes/budget_routes.py
+++ Backend/app/routes/budget_routes.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from fastapi.responses import JSONResponse
 from app.services.budget_service import BudgetService
 from app.connection.app_database_connection import app_session
+from app.schemas.budget_schema import CreateBudgetSchema
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 budget_router = APIRouter()
 budget_service = BudgetService()
 
-@budget_router.get("/", status_code=status.HTTP_200_OK)
-async def get_all_budgets()
+@budget_router.get("/", status_code=status.HTTP_200_OK, response_model=CreateBudgetSchema)
+async def get_all_budgets(session: AsyncSession = Depends(app_session)):
+    
--- Backend/app/schemas/budget_schema.py
+++ Backend/app/schemas/budget_schema.py
@@ -2,6 +2,15 @@ from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Any, Dict
 
+# for fetching the record
+class BudgetSchema(BaseModel):
+    item_name: str = Field(min_length=1)
+    item_type: Dict[str, Any]
+    purchase_date: datetime = Field(default_factory=lambda: datetime.now)
+    description: str = Field(min_length=1, max_length=100)
+    created_at: datetime = Field(default_factory=lambda: datetime.now)
+
+# for creating a new record
 class CreateBudgetSchema(BaseModel):
     item_name: str = Field(min_length=1)
     item_type: Dict[str, Any]

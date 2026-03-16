3e9b193e2d57de9fc3f4747598f0a9e12015e83f
Martin Silungwe <martinsilungwe12@gmail.com>
2026-03-16 10:57:25 +0200

    working on the budget services

---
+++ Backend/app/services/budget_service.py
@@ -0,0 +1,17 @@
+from sqlmodel.ext.asyncio.session import AsyncSession
+from app.models.budget import BudgetModel
+from app.schemas.budget_schema import CreateBudgetSchema
+
+
+class BudgetSchema:
+
+    async def create_budget(self, budget_data: CreateBudgetSchema, session: AsyncSession):
+        budget = budget_data.model_dump()
+        new_budget = BudgetModel(**budget)
+        if new_budget is not None:
+            session.add(new_budget)
+            await session.commit()
+            await session.refresh(new_budget)
+            return new_budget
+        else:
+            return None

f78c087c934297c4893a903527a0fa7d329e35b1
Martin Silungwe <martinsilungwe12@gmail.com>
2026-03-16 10:42:21 +0200

    adding budget models

--- Backend/app/models/budget.py
+++ Backend/app/models/budget.py
@@ -1,6 +1,9 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Column
 import uuid
 from sqlalchemy import JSON
+from typing import Dict, Any
+from datetime import datetime
+
 
 class BudgetModel(SQLModel, table=True):
 
@@ -8,4 +11,7 @@ class BudgetModel(SQLModel, table=True):
 
     budget_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, nullable=False)
     item_name: str = Field(min_length=1, index=True, nullable=False)
-    item_type: str = Field(min_length=)
+    item_type: Dict[str, Any] = Field(sa_column=Column(JSON))
+    purchase_date: datetime = Field(default_factory=lambda : datetime.now)
+    description: str = Field(min_length=1, max_length=100, nullable=False)
+    created_at: datetime = Field(default_factory=lambda : datetime.now)

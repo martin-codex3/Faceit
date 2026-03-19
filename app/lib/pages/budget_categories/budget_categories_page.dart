6e91b5b00768ed859e7a000f3822eba5e896f78f
Martin Silungwe <martinsilungwe12@gmail.com>
2026-03-19 14:10:45 +0200

    adding the categories

--- app/lib/pages/budget_categories/budget_categories_page.dart
+++ app/lib/pages/budget_categories/budget_categories_page.dart
@@ -1,8 +1,10 @@
 import 'package:app/controllers/budget_controller/budget_categories_controller.dart';
 import 'package:app/shared/styled_body.dart';
 import 'package:app/shared/styled_title.dart';
+import 'package:app/themes/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 
 class BudgetCategoriesPage extends ConsumerStatefulWidget {
   const BudgetCategoriesPage({super.key});
@@ -29,25 +31,108 @@ class _BudgetCategoriesPageState extends ConsumerState<BudgetCategoriesPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   if (data[index].title == "Food & Groceries")
-                    Text(data[index].title),
+                    Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          TablerIcons.burger,
+                          color: AppColors.primaryWhite,
+                          size: 30,
+                        ),
+                        Text(data[index].title),
+                      ],
+                    ),
 
-                  if (data[index].title == "Transport") Text(data[index].title),
+                  if (data[index].title == "Transport")
+                    Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          TablerIcons.car,
+                          color: AppColors.primaryWhite,
+                          size: 30,
+                        ),
+                        Text(data[index].title),
+                      ],
+                    ),
 
                   if (data[index].title == "Housing & Utilities")
-                    Text(data[index].title),
+                    Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          TablerIcons.solar_electricity,
+                          color: AppColors.primaryWhite,
+                          size: 30,
+                        ),
+                        Text(data[index].title),
+                      ],
+                    ),
 
                   if (data[index].title == "Health & Wellness")
-                    Text(data[index].title),
+                    Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          TablerIcons.report_medical,
+                          color: AppColors.primaryWhite,
+                          size: 30,
+                        ),
+                        Text(data[index].title),
+                      ],
+                    ),
 
-                  if (data[index].title == "Shopping") Text(data[index].title),
+                  if (data[index].title == "Shopping")
+                    Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          TablerIcons.shopping_bag,
+                          color: AppColors.primaryWhite,
+                          size: 30,
+                        ),
+                        Text(data[index].title),
+                      ],
+                    ),
 
                   if (data[index].title == "Entertainment")
-                    Text(data[index].title),
+                    Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          TablerIcons.device_tv_old,
+                          color: AppColors.primaryWhite,
+                          size: 30,
+                        ),
+                        Text(data[index].title),
+                      ],
+                    ),
 
-                  if (data[index].title == "Education") Text(data[index].title),
+                  if (data[index].title == "Education")
+                    Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          TablerIcons.book,
+                          color: AppColors.primaryWhite,
+                          size: 30,
+                        ),
+                        Text(data[index].title),
+                      ],
+                    ),
 
                   if (data[index].title == "Savings & Financial")
-                    Text(data[index].title),
+                    Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          TablerIcons.moneybag,
+                          color: AppColors.primaryWhite,
+                          size: 30,
+                        ),
+                        Text(data[index].title),
+                      ],
+                    ),
 
                   Wrap(
                     spacing: 8,
